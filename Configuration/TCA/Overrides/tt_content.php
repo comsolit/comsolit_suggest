c670a52d534c77d5d5d92ea29862a22eac86575f
Maurizio Silipo <silipo@comsolit.com>
2017-12-13 13:47:26 +0100

    override sys_template and tt_content instead of writing tca globals directly in ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'comsolit_suggest',
+    'Suggest',
+    'comsolit Suggest'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,2 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Suggest',
-	'comsolit Suggest'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'comsolit Suggest');
+defined('TYPO3_MODE') or die();
