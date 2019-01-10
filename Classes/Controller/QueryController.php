791edc6a49a5658999be9205c119d1a498344de1
Tristan Weissmann <weissmann@comsolit.com>
2019-01-10 15:48:13 +0100

    [INT-7220] security fix; version update to 1.1.1;

--- Classes/Controller/QueryController.php
+++ Classes/Controller/QueryController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Comsolit\ComsolitSuggest\Controller;
 
 
@@ -30,56 +31,61 @@ namespace Comsolit\ComsolitSuggest\Controller;
 /**
  * QueryController
  */
-class QueryController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+class QueryController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
 
-	/**
-	 * queryRepository
-	 *
-	 * @var \Comsolit\ComsolitSuggest\Domain\Repository\QueryRepository
-	 * @inject
-	 */
-	protected $queryRepository = NULL;
+    /**
+     * queryRepository
+     *
+     * @var \Comsolit\ComsolitSuggest\Domain\Repository\QueryRepository
+     * @inject
+     */
+    protected $queryRepository = NULL;
 
-	/**
-	 * action suggest
-	 *
-	 * @return void
-	 */
-	public function suggestAction() {
-		
-		if($this->request->hasArgument('search')) {
-			$search = $this->request->getArgument('search');
-			
-			$suggestions = [];
-			$language = $GLOBALS['TSFE']->sys_language_uid;
-			$field ='SQL_NO_CACHE DISTINCT baseword';
-			$from    ='index_words LEFT JOIN index_rel ON index_words.wid = index_rel.wid 
+    /**
+     * action suggest
+     *
+     * @return void
+     */
+    public function suggestAction()
+    {
+
+        if ($this->request->hasArgument('search')) {
+
+            $search = $GLOBALS['TYPO3_DB']->quoteStr($this->request->getArgument('search'), 'index_words');
+
+            $suggestions = [];
+            $language = $GLOBALS['TSFE']->sys_language_uid;
+            $field = 'SQL_NO_CACHE DISTINCT baseword';
+            $from = 'index_words LEFT JOIN index_rel ON index_words.wid = index_rel.wid 
 						LEFT JOIN index_phash ON index_rel.phash = index_phash.phash';
-			$where  ='baseword LIKE '."'" . $search . '%' ."'" . ' AND index_phash.sys_language_uid =' . $language;
-			$groub_by = '';
-			$order_by = '';
-			$limit = '10';
-			$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($field, $from, $where, $groub_by, $order_by, $limit);
-			while($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)){
-				$suggestions[] = $row; 
-			}
-			
-			return $this->buildJsonRepsonseFromQuery($suggestions);
-		}
-	}
-	
-	private function buildJsonRepsonseFromQuery($suggestions) {
-		return json_encode( $this->createValueMapFromStringArray($suggestions));
-	}
-	
-	private function createValueMapFromStringArray($array) {
-		$options = [];
-		foreach($array as $value){
-			if(!is_int($value)) {
-				$options[]['value'] = $value['baseword'];
-			}
-		}
-		return $options;
-	}
+            $where = 'baseword LIKE ' . "'" . $search . '%' . "'" . ' AND index_phash.sys_language_uid =' . $language;
+            $groub_by = '';
+            $order_by = '';
+            $limit = '10';
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($field, $from, $where, $groub_by, $order_by, $limit);
+            while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+                $suggestions[] = $row;
+            }
+
+            return $this->buildJsonRepsonseFromQuery($suggestions);
+        }
+    }
+
+    private function buildJsonRepsonseFromQuery($suggestions)
+    {
+        return json_encode($this->createValueMapFromStringArray($suggestions));
+    }
+
+    private function createValueMapFromStringArray($array)
+    {
+        $options = [];
+        foreach ($array as $value) {
+            if (!is_int($value)) {
+                $options[]['value'] = $value['baseword'];
+            }
+        }
+        return $options;
+    }
 
 }
