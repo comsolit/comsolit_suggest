7f2627621e624276b74727b2e636f61a4b142a4b
akoenig <koenig@clickstorm.de>
2022-02-18 15:45:01 +0100

    [TASK] reformat

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,9 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Comsolit.comsolit_suggest',
-	'Suggest',
+    'Comsolit.comsolit_suggest',
+    'Suggest',
     [
         \Comsolit\ComsolitSuggest\Controller\QueryController::class => 'suggest',
-	]
+    ]
 );
